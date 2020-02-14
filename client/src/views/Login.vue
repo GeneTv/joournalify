2fe305fa286f653ff6f6859a91bb6a06720ca28b
GeneTv <lpgenehd@gmail.com>
2020-02-15 00:40:24 +0100

    ---

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,10 +1,69 @@
 <template>
-  
+  <v-content>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-layout
+          align-center
+          justify-center
+        >
+          <v-flex
+            xs12
+            sm8
+            md4
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Login required</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Username"
+                    name="username"
+                    prepend-icon="person"
+                    type="email"
+                    v-model="inputUsername"
+                  ></v-text-field>
+
+                  <v-text-field
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                    v-model="inputPassword"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" v-on:click="login()">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      inputUsername: "u",
+      inputPassword: "p"
+    }
+  },
+  methods: {
+    login() {
+      console.log(`Try login as ${this.inputUsername} with password ${this.inputPassword}`)
+    }
+  }
 }
 </script>
 
