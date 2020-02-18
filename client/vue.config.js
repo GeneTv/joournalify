70a649d2c808b9cd4e126a83631b1cb5d80fd052
GeneTv <lpgenehd@gmail.com>
2020-02-18 12:45:36 +0100

    ???

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ],
-  pwa: {
-    name: 'Joournalify',
-    themeColor: '#1976D2',
-    msTileColor: '#1976D2',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#1976D2'
-  }
+  ]
 }
